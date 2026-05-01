57ae44a327cd6532da2d7b349306f37b71db5b37
Arif-Sofi <muhammad.awparif.hakimi@gmail.com>
2026-05-02 00:09:43 +0800

    Update router.dart
    update auto redirect to /login if not logged in

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../login_screen.dart';
 import '../home_screen.dart';
@@ -7,24 +10,45 @@ import '../forgot_password_screen.dart';
 import '../register_screen.dart';
 import '../profile_screen.dart';
 
-
 class AppRouter {
   final GoRouter router = GoRouter(
+    initialLocation: '/',
+    refreshListenable: GoRouterRefreshStream(FirebaseAuth.instance.authStateChanges()), //tells Gorouter to listen to change, to rerun redirect logic
+    redirect: (BuildContext context, GoRouterState state) {
+      final bool loggedIn = FirebaseAuth.instance.currentUser != null;
+      final bool loggingIn = state.matchedLocation == '/login';
+      final bool registering = state.matchedLocation == '/register';
+      final bool forgettingPassword = state.matchedLocation == '/forgot-password';
+
+      if(!loggedIn){
+        //redirect to /login if not logged in and not on public page
+        if(!loggingIn && !registering && !forgettingPassword){
+          return '/login';
+        }
+      }
+      else {
+        if (loggingIn) {
+          return '/';
+        }
+      }
+      return null;
+    },
+
     routes: <RouteBase>[
+      GoRoute(
+        path: '/login',
+        builder: (BuildContext context, GoRouterState state) {
+          return const LoginScreen();
+        }
+      ),
+
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
-          return const LoginScreen();
+          return const HomeScreen();
         },
 
         routes: <RouteBase>[
-          GoRoute(
-            path: 'home',
-            builder: (BuildContext context, GoRouterState state) {
-              return const HomeScreen();
-            }
-          ),
-
           GoRoute(
             path: 'forgot-password',
             builder: (BuildContext context, GoRouterState state) {
@@ -49,4 +73,22 @@ class AppRouter {
       )
     ]
   );
+}
+
+//This class listens to the stream of auth changes and notifies GoRouter to recheck the redirect logic whenever there is a change (like login/logout)
+//uesd in first GoRouter
+class GoRouterRefreshStream extends ChangeNotifier {
+  GoRouterRefreshStream(Stream<dynamic> stream) {
+    notifyListeners();
+    _subscription = stream.asBroadcastStream().listen(
+      (dynamic _) => notifyListeners(),
+    );
+  }
+
+  late final StreamSubscription<dynamic> _subscription;
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
 }
