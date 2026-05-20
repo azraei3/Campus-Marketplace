8b42767ae6f2c501abdb150c4f832fdbfad0b939
Arif-Sofi <muhammad.awparif.hakimi@gmail.com>
2026-05-20 12:27:56 +0800

    update Profile Page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:go_router/go_router.dart';
 
 import 'drawer_items.dart';
 
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:go_router/go_router.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -63,7 +64,87 @@ class ProfileScreen extends StatelessWidget {
                 ),
                 Text(
                   userData['email'] ?? 'No Email',
-                  style: Theme.of(context).textTheme.titleLarge,
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: Wrap(
+                    alignment: WrapAlignment.spaceBetween,
+                    spacing: 20,
+                    runSpacing: 20,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        width: 175,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), border: Border.all(color: Colors.grey, width: 2.0)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.favorite_border_rounded, size: 40),
+                            const SizedBox(height: 5),
+                            const Text('Saved Items', style: TextStyle(fontSize: 20))
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        width: 175,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), border: Border.all(color: Colors.grey, width: 2.0)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.inbox_outlined, size: 40),
+                            const SizedBox(height: 5),
+                            const Text('Inbox', style: TextStyle(fontSize: 20))
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        width: 175,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), border: Border.all(color: Colors.grey, width: 2.0)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.settings, size: 40),
+                            const SizedBox(height: 5),
+                            const Text('Settings', style: TextStyle(fontSize: 20))
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        width: 175,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), border: Border.all(color: Colors.grey, width: 2.0)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.history, size: 40),
+                            const SizedBox(height: 5),
+                            const Text('Recently Viewed', style: TextStyle(fontSize: 20), overflow: TextOverflow.ellipsis, maxLines: 1,)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+                InkWell(
+                  onTap: () async {
+                    await FirebaseAuth.instance.signOut();
+                    if (context.mounted){
+                      context.go('/login');
+                    }
+                  },
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(20), border: Border.all(color: Colors.black, width: 2.0)),
+                    child: Column(children: [Text('Logout', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.white))],)
+                  ),
                 ),
               ],
             ),
